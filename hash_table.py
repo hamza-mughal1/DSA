a9d7b2b62793e1f6484f7a58c73c42c6a5f316ff
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-09-21 15:33:50 +0500

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -8,22 +8,25 @@ class HashTable:
         for i in string:
             num+=ord(i)
         num *= len(string)
-        return num % 100
+        return num % self.lenght
 
     def __setitem__(self,key,value):
-        self.arr[self.hash_func(key)] = value
+        self.arr[self.hash_func(key)].append((key,value))
         return
     
     def __getitem__(self,key):
-        return self.arr[self.hash_func(key)]
+        for i in self.arr[self.hash_func(key)]:
+            if i[0] == key:
+                return i[1]
+        
 
     def insert(self,lis):
         for i in lis:
-            self.arr[self.hash_func(i[0])] = i[1]
+            self.__setitem__(i[0],i[1])
         return
     
 
-hash = HashTable(100)
+hash = HashTable(10)
 
 hash.insert([("hamza",18),
              ("hamzaa",19),
